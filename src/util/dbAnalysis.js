bef7756675ce7a84338be99708c89f01a21e0ee9
EduRomoGO <eduromogo@outlook.es>
2020-01-31 16:56:12 +0000

    feat: create analysis file to check db items status

---
+++ src/util/dbAnalysis.js
@@ -0,0 +1,53 @@
+const { db } = require('../dbCreator.js');
+
+const allDescriptors = db.get('wordDescriptors')
+    .value()
+
+const isPhonemicsIncomplete = item => (item.phonemics.match(/\//g) || []).length < 2;
+
+const toInvalidReason = (acc, next) => {
+    let reason;
+
+    if (next.definitions.length === 0) {
+        reason = 'no-definitions';
+    } else if (next.phonemics === undefined || next.phonemics.trim().length === 0) {
+        reason = 'missing-phonetics';
+    } else if (isPhonemicsIncomplete(next)) {
+        reason = 'wrong-phonetics';
+    } else if (next.url === '') {
+        reason = 'missing-audio-url';
+    }
+
+    return {...acc, [next.word]: reason };
+}
+
+const isInvalid = item => {
+    let isInvalid = false;
+
+    if (item.definitions.length === 0) {
+        isInvalid = true;
+    } else if (item.phonemics === undefined || item.phonemics.trim().length === 0) {
+        isInvalid = true;
+    } else if (isPhonemicsIncomplete(item)) {
+        isInvalid = true;
+    } else if (item.url === '') {
+        isInvalid = true;
+    }
+    
+    return isInvalid;
+};
+
+const performAnalysis = () => {
+    return allDescriptors
+        .filter(isInvalid)
+        .reduce(toInvalidReason, {});
+};
+
+const results = performAnalysis();
+
+const getWrongWords = () => Object.keys(results);
+
+// console.log(results);
+// console.log(Object.keys(results).length);
+
+module.exports = { getWrongWords };
