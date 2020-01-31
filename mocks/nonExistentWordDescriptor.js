011656297fd1f05f25d325cec86d7818d5fe7bc7
EduRomoGO <eduromogo@outlook.es>
2020-01-31 11:00:39 +0000

    feat: add mock

---
+++ mocks/nonExistentWordDescriptor.js
@@ -0,0 +1,10 @@
+const nonExistentWordDescriptor = {
+    "word": "romo",
+    "phonemics": "",
+    "definitions": [],
+    "protocol": "https",
+    "extension": "mp3",
+    "url": "https://www.wordreference.com/audio/en/uk/general/"
+};
+
+module.exports = { nonExistentWordDescriptor };
