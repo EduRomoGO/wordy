58d6015b8e73e42053d288a0cc2e93db9ce07168
EduRomoGO <eduromogo@outlook.es>
2020-01-31 18:19:00 +0000

    feat: more info

--- index.js
+++ index.js
@@ -15,19 +15,25 @@ const isNotInDb = word => {
 
 const getWordsInfo = words => {
     let timeoutAcc = 0;
+    let inDb = 0;
 
     // Make sure that calls are sequential (one every 20-30 secs) to avoid penalizations from wr
     words.forEach(word => {
         if (isNotInDb(word)) {
             setTimeout(async () => {
                 console.log(`Start searching for word "${word}"`);
-                const msg = await getWordInfo(word);
-
-                console.log(msg);
+                try {
+                    const msg = await getWordInfo(word);
+                    console.log(msg);
+                } catch (error) {
+                    console.log(error);
+                }
             }, timeoutAcc);
         
             timeoutAcc += getWaitSeconds() * 1000;
         } else {
+            inDb++;
+            console.log(`${inDb} words already in db`);
             console.log(`"${word}" is already present in db`);
         }
     });
