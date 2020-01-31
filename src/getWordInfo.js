7ce8d735b5aeb77cbe3ce8a4099c02068fac7062
EduRomoGO <eduromogo@outlook.es>
2020-01-31 11:15:33 +0000

    feat: store only words with definitions in db

--- src/getWordInfo.js
+++ src/getWordInfo.js
@@ -15,7 +15,6 @@ const createDescriptor = ({audio, phonemics, definitions, word}) => {
         definitions,
         protocol: 'https',
         extension: 'mp3',
-        idAudio,
         url: `https://www.wordreference.com/audio/en/uk/general/${idAudio}`,
     };
 };
@@ -29,18 +28,23 @@ const saveDescriptorToDb = descriptor => {
 const getWordInfo = async word => {
     try {
         const data = await searchWord(word);
-        const descriptor = createDescriptor({...data, word});
-
-        saveDescriptorToDb(descriptor);
-        getAudioFile(descriptor);
-
-        console.log('=====================')
-        console.log(`Successfully searched ${word}, created its descriptor, saved it to db and retrieved its audio file`);
-        console.log('=====================')
-        console.log(' ');
-        console.log(' ');
+        
+        if (data.definitions.length > 0) {
+            const descriptor = createDescriptor({...data, word});
+            
+            saveDescriptorToDb(descriptor);
+            getAudioFile(descriptor);
+            
+            console.log('=====================')
+            console.log(`Successfully searched ${word}, created its descriptor, saved it to db and retrieved its audio file`);
+            console.log('=====================')
+            console.log(' ');
+            console.log(' ');
+        } else {
+            console.log(`Word "${word}" has not any definitions, so it wont be stored in the db`);
+        }
     } catch (error) {
-        console.error(error);
+        throw new Error(`Error while getting word info for word: "${word}" --> ${error}`);
     }
 };
 
