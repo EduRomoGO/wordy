708464c4b25bf01a583b4f9d4513505df8ede191
EduRomoGO <eduromogo@outlook.es>
2020-02-01 19:52:03 +0000

    feat: feed db

--- index.js
+++ index.js
@@ -47,6 +47,30 @@ const getWordsInfo = words => {
 };
 
 // const words = getWrongWords();
-const words = allWords.slice(1000, 1500);
+const words = allWords.slice(1500, 1800);
 
 getWordsInfo(words);
+
+
+
+function run () {
+    const soundList = [];
+    const createId = i => {
+        if (i < 11) {
+            return `sym_000${i}`;
+        } else {
+            return `sym_00${i}`;
+        }
+    };
+
+    for (let i = 0; i < 100; i++) {
+        const soundId = createId(i);
+
+        if (document.querySelector(`#${soundId}`)) {
+            soundList.push(soundId);
+        }
+    }
+
+    return soundList;
+}
+
