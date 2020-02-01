5b866d6d1f9f9aa5c112cb82d22efd679e2612d8
EduRomoGO <eduromogo@outlook.es>
2020-02-01 13:46:30 +0000

    refactor: reorder code

--- scripts/removeWrongWords.js
+++ scripts/removeWrongWords.js
@@ -1,5 +1,5 @@
-const { db } = require('../dbCreator.js');
-const { getWrongWords } = require('./dbAnalysis.js');
+const { db } = require('../src/dbCreator.js');
+const { getWrongWords } = require('../src/util/dbAnalysis.js');
 
 getWrongWords().forEach(word => {
     db.get('wordDescriptors')
