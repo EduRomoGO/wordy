f17417a026083272b04861072f42b58793832e77
Raquel <raquelromo@samyroad.com>
2020-02-02 01:58:09 +0100

    undo changes at playFile

--- scripts/playFile.js
+++ scripts/playFile.js
@@ -4,14 +4,7 @@ const player = require('play-sound')(opts = {})
 //     if (err) throw err
 // });
 
-// player.play('./audioFiles/blue.mp3', function(err){
-//     if (err) throw err
-// });
-
-player.play('../db/phonemesFiles/ability.mp3', function(err){
+player.play('./audioFiles/blue.mp3', function(err){
     if (err) throw err
 });
 
-// player.play('../db/phonemesFiles/phonem.mp3', function(err){
-//     if (err) throw err
-// });
