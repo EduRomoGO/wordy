283ba1d254ecf9c4ae9fe920eb144249d34408cf
Raquel <raquelromo@samyroad.com>
2020-02-04 00:46:22 +0100

    fix audios

--- src/getPhonemeFile.js
+++ src/getPhonemeFile.js
@@ -18,13 +18,14 @@ const getAudioFile = ({ title, url }) => {
 // getAudioFile({url: 'http://cambridgeenglishonline.com/interactive_phonemic_chart/snd/sym/img_0024.mp3'})
 // getAudioFile({url: 'http://cambridgeenglishonline.com/interactive_phonemic_chart/snd/word/img_0024.mp3'})
 
-const getPhonemesListFiles = (items) => {
+const getPhonemesListFiles = (items, type) => {
     items.map((item, index)=> {
-    const file = index < 10 ? `000${index}`:`00${index}`
+    const newIndex = index +1
+    const file = newIndex < 10 ? `000${newIndex}`:`00${newIndex}`
     return (
         getAudioFile({
             title: `${item}`,
-            url:`http://cambridgeenglishonline.com/interactive_phonemic_chart/snd/word/img_${file}.mp3`
+            url:`http://cambridgeenglishonline.com/interactive_phonemic_chart/snd/${type}/img_${file}.mp3`
         })
     )})
 }
@@ -32,7 +33,7 @@ const getPhonemesListFiles = (items) => {
 const phonems = ['p', 'b', 't', 'd','k', 'g', 'm', 'n','ŋ', 'f', 'v', 'θ','ð', 's', 'z', 'ʃ','ʒ', 'h', 'tʃ', 'dʒ', 'r', 'l', 'j', 'w', 'i:','ɪ','e','æ', 'ɑ:', 'ʌ','ɒ','ɔ:','ʊ','u:','ɜ:ʳ','ə','eɪ','aɪ','ɔɪ','oʊ','aʊ','eəʳ','ɪəʳ','ʊə']
 const words = ['soup', 'bat', 'light', 'dog','cap', 'bag', 'man', 'rain','king', 'fish', 'van', 'thumb','mother', 'mouse', 'zebra', 'shoe','television', 'hand', 'cheese', 'jet', 'write', 'long', 'yo-yo', 'whale', 'sea','swim','bed','cat', 'car', 'cup','lock','ball','book','two','shirt','father','eight','eye','boy','arrow','house','chair','deer','tourist']
 
-getPhonemesListFiles(phonems)
-getPhonemesListFiles(words)
+getPhonemesListFiles(phonems, 'sym')
+getPhonemesListFiles(words, 'word')
 
 module.exports = { getAudioFile };
