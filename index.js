c2eba57d28338cba472cfa31e20362cc92a91d3e
EduRomoGO <eduromogo@outlook.es>
2020-02-05 23:06:27 +0000

    feat: feed db

--- index.js
+++ index.js
@@ -52,6 +52,6 @@ const getWordsInfo = words => {
 };
 
 // const words = getWrongWords();
-const words = allWords.slice(3800, 4500);
+const words = allWords.slice(4350, 4500);
 
 getWordsInfo(words);
