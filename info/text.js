650e5331e17c6ebead0a89b0992d608114fbca2c
EduRomoGO <eduromogo@outlook.es>
2020-11-03 16:33:55 +0100

    docs: update

---
+++ info/text.js
@@ -0,0 +1,83 @@
+module.exports = {
+    text: `Edu Romo Gómez
+    ======
+    
+    **Full Stack Js Developer**
+    
+    *Home Location:*
+    Quaker Street, London
+    
+    *Contact Details:*
+    
+    ✉️ eduromogo@outlook.es   
+    📱 +34 677 04 36 52
+    
+    
+    ### **Summary**
+    
+    I have worked for startup, medium sized and big companies and I have learned a lot from all of them. I have become a frontend specialist with 8+ years of experience building web applications and have acquired a strong set of tools and technologies to bring success into any project. I enjoy creating amazing products that increases company revenue, improves customer satisfaction, or reduce company expenses.
+    
+    If you hire me, you get someone that:
+    
+    - Has a strong set of skills and technologies and new ones coming every day
+    - Understands that technologies are tools to build a product that fit client needs, not a toy to play with
+    - Has clear priorities towards company goals
+    - Is not afraid of challenges
+    - Is a fast learner
+    - Knows how to comunicate effectively
+    - Is a team player
+    - Is honest, commited and hard worker
+    - Has a positive attitude
+    - Is versatile regarding tech stuff, frontend specialist, but with a decent background in the backend side (node/express/mongo)
+    - Keeps a mentoring attitude towards junior developers
+    - Understands business needs and knows how to translate them into product features
+    
+    
+    **Experience**
+    --------------
+    
+    #### **Senior Front-end Developer and Scrum Master at ING**
+    
+    *February 2017 - January 2020*
+    
+    Build ING web application for customers to bring them a modern and faster experience.
+    
+    #### **Full-stack Developer at DOCOMO Digital**
+    
+    *October 2015 - January 2017*
+    
+    Improved marketing campaign tool to create campaigns faster.
+    Built dashboard to pinpoint anomalies in marketing campaigns that helped prevent fraud.
+    
+    #### **Freelance Web Developer and Designer at Aparky**
+    
+    *January 2015 - September 2015*
+    
+    Built a web app for private parking renting from scratch which meant all the business for the startup.
+    
+    #### **Software Developer at everis**
+    
+    *2011 - 2014*
+    
+    Built customer care tool for Telefonica that increased client satisfaction.
+    Built control tool for Enagas that enabled increased control of gas flow inside the net.
+    
+    
+    **Skills and technologies**
+    ---------------------------
+    
+    | Technologies 	                                   | Industry            	| Tools & Workflow            	|
+    |--------------	                                   |---------------------	|-----------------------------	|
+    | JavaScript / HTML / CSS (8+ years)               | Agile Methodologies 	| Git                         	|
+    | React / Redux (5+ years)                         | Scrum               	| TDD                         	|
+    | Node / Express / REST / MongoDB (2+ years)       | Web development     	| Object Oriented Programming 	|
+    |           	                                   |                     	| Functional Programming      	|
+    |            	                                   |                     	| Nginx                       	|
+    |           	                                   |                     	| Jenkins                     	|
+    
+    
+    ## **Languages**
+    
+    **Spanish** - Native speaker  
+    **English** - Complete professional knowledge`
+};
